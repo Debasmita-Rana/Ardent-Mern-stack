fb67a9f52cd467bdc6953c13fa9d1371f54dbb56
Debasmita-Rana <debasmitarana04@gmail.com>
2026-06-10 07:26:45 +0530

    backend and frontend

---
+++ Final project/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Home } from './components/Home'
+
+const App = () => {
+  return <>
+  <div>
+    <Header />
+    <Footer />
+    <Home />
+    </div>
+    </>
+  
+}
+
+export default App
