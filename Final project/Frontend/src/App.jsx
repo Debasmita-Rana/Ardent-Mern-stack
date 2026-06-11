0d2ccfa84d2ca965e30f53313061253c1d5ccbdb
Debasmita-Rana <debasmitarana04@gmail.com>
2026-06-11 21:28:18 +0530

    updated code

--- Final project/Frontend/src/App.jsx
+++ Final project/Frontend/src/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Home } from './components/Home'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import  Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Addcourse from './components/Addcourse';
+import Coursedetails from './components/Coursedetails';
 const App = () => {
   return <>
-  <div>
-    <Header />
-    <Footer />
-    <Home />
-    </div>
-    </>
-  
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About message="ok"/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/add" element={<Addcourse/>}></Route>
+        <Route path="/courses" element={<Coursedetails/>}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </>
 
-export default App
+};
+
+export default App;
---
+++ Final project/Frontend/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = (props) => {
+  return <>
+  message: {props.message}
+  
+  </>
+  
+}
+
+export default About
