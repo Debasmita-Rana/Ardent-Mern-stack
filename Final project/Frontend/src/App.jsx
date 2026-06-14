a37310f6353b3dde719d726ea9f694b216a3e1c4
Debasmita-Rana <debasmitarana04@gmail.com>
2026-06-14 09:23:05 +0530

    backend updated

--- Final project/Frontend/src/App.jsx
+++ Final project/Frontend/src/App.jsx
@@ -7,16 +7,20 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Addcourse from './components/Addcourse';
 import Coursedetails from './components/Coursedetails';
+import Editcourse from './components/Editcourse';
+import Viewcourse from './components/Viewcourse';
 const App = () => {
   return <>
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home/>}></Route>
-        <Route path="/about" element={<About message="ok"/>}></Route>
+        <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
         <Route path="/add" element={<Addcourse/>}></Route>
-        <Route path="/courses" element={<Coursedetails/>}></Route>
+        <Route path="/courses/:id" element={<Coursedetails/>}></Route>
+        <Route path="/edit-courses/:id" element={<Editcourse/>}></Route>
+         <Route path="/courses" element={<Viewcourse/>}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
--- Final project/backend/server.js
+++ Final project/backend/server.js
@@ -2,19 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectdb = require('./config/db');
+const courseroutes = require('./routes/courseroutes');
 
 
 dotenv.config();
 const app = express();
 connectdb();
-
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/courses", courseroutes);
 app.get('/',(req,res)=>{
     res.send("api is working");
 });
 const port = process.env.port;
+
 app.listen(port,()=>{
     console.log("server is running port 5500");
 });
